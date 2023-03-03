016b3dbedd9be539b2c28b06b0f8b8827bcdf597
Ashish Sharda <ashishjsharda@gmail.com>
2023-03-03 05:36:57 -0600

    add ratelimiter

--- src/main/java/config/RateLimiterConfiguration.java
+++ src/main/java/config/RateLimiterConfiguration.java
@@ -1,8 +1,9 @@
 package config;
+import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.time.Duration;
 
 @Configuration
@@ -15,5 +16,15 @@ public class RateLimiterConfiguration {
                 .limitForPeriod(1)
                 .timeoutDuration(Duration.ofSeconds(1))
                 .build();
+    }
+
+    @Bean
+    public RateLimiterRegistry rateLimiterRegistry(RateLimiterConfig rateLimiterConfig) {
+        return RateLimiterRegistry.of(rateLimiterConfig);
+    }
+
+    @Bean
+    public RateLimiter rateLimiter(RateLimiterRegistry rateLimiterRegistry) {
+        return rateLimiterRegistry.rateLimiter("backendA");
     }
 }
